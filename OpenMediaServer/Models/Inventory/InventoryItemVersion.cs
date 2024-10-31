ab83a7f3cae2680d4bfa7dd6398cc4b30f0507de
redinsch <redinsch@gmx.de>
2024-11-01 00:37:03 +0100

    :children_crossing: added optional Version Property "Name" to InventoryItemVersion

--- OpenMediaServer/Models/Inventory/InventoryItemVersion.cs
+++ OpenMediaServer/Models/Inventory/InventoryItemVersion.cs
@@ -5,4 +5,5 @@ public class InventoryItemVersion
     public Guid Id { get; set; }
     public string Path { get; set; }
     public Guid? FileInfoId { get; set; }
+    public string? Name { get; set; }
 }
