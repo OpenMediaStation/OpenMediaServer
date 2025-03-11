c3865482bc9eed9d564fc1a594db60387efddd63
Lukas Nagel <github@lna-dev.net>
2025-03-11 21:53:04 +0100

    Better audiobook discovery

--- OpenMediaServer/Services/Discovery/DiscoveryAudiobookService.cs
+++ OpenMediaServer/Services/Discovery/DiscoveryAudiobookService.cs
@@ -13,7 +13,6 @@ public class DiscoveryAudiobookService : IDiscoveryAudiobookService
     private readonly IFileInfoService _fileInfoService;
     private readonly IInventoryService _inventoryService;
     private readonly IMetadataService _metadataService;
-    private readonly string _regex = @"(?<category>(Audiobooks)|\w+?)/.*?(?<folderTitle>[ \w.-]*?)?((\(|\.)(?<yearFolder>\d{4})(\)|\.?))?/?/?(?<title>([ \w\.-]+?))((\(|\.)(?<year>\d{4})(\)|\.?))?((-|\.)(?<fileInfo>[\w\.-]*?))?\.(?<extension>\S{3,4})$";
 
     public DiscoveryAudiobookService(ILogger<DiscoveryAudiobookService> logger, IFileInfoService fileInfoService, IInventoryService inventoryService, IMetadataService metadataService)
     {
@@ -27,89 +26,103 @@ public class DiscoveryAudiobookService : IDiscoveryAudiobookService
     {
         _logger.LogTrace("Creating audiobook for path: {Path}", path);
 
-        var pathRegex = new Regex
-        (
-            pattern: _regex,
-            options: RegexOptions.Compiled
-        );
-
-        var match = pathRegex.Match(path.Replace(Globals.MediaFolder, string.Empty));
-        if (!match.Success)
+        if (IsPart(path))
         {
-            _logger.LogWarning("Invalid path: {Path}", path);
-            return;
+
         }
-        var groups = match.Groups;
-        var folderTitle = groups["folderTitle"].Value;
-        var title = groups["title"].Value;
-        var extension = groups["extension"].Value;
-
-        var books = await _inventoryService.ListItems<Audiobook>("Audiobook");
-        var existingBooks = books?.Where(i => i.Versions?.Any(i => i.Path == path) ?? false).FirstOrDefault();
-
-        string? folderPath = null;
-
-        if (!string.IsNullOrEmpty(folderTitle))
+        else
         {
-            folderPath = path.Replace($"/{title}.{extension}", "");
-            existingBooks = books?.Where(i => i.FolderPath == folderPath).FirstOrDefault();
-        }
+            var splittedPath = path.Split("/");
 
-        if (existingBooks != null)
-        {
-            if (!string.IsNullOrEmpty(folderTitle))
+            var folderTitle = (splittedPath.Length - 2) >= 0 ? splittedPath[^2] : null;
+            var extension = splittedPath.LastOrDefault()?.Split(".").LastOrDefault();
+
+            var splittedTitle = splittedPath.LastOrDefault()?.Split(".").SkipLast(1);
+
+            if (splittedTitle == null)
             {
-                var version = new InventoryItemVersion
-                {
-                    Id = Guid.NewGuid(),
-                    Path = path,
-                };
-
-                if (existingBooks.Versions?.Any(i => i.Path == path) ?? false)
-                {
-                    return;
-                }
-
-                // Do this after the path check because a file info will be created
-                version.FileInfoId = (await _fileInfoService.CreateFileInfo(path, version.Id, "Audiobook"))?.Id;
-
-                existingBooks.Versions = existingBooks.Versions?.Append(version);
-
-                await _inventoryService.UpdateByTitle(existingBooks);
+                _logger.LogWarning("SplittedTitle null.... Invalid path: {Path}", path);
+                return;
             }
 
-            return;
-        }
+            var title = string.Join(".", splittedTitle);
 
-        var versionId = Guid.NewGuid();
-        var book = new Audiobook()
-        {
-            Id = Guid.NewGuid(),
-            Versions =
-            [
-                new()
+            if (extension == null || title == null)
+            {
+                _logger.LogWarning("Invalid path: {Path}", path);
+                return;
+            }
+
+            var books = await _inventoryService.ListItems<Audiobook>("Audiobook");
+            var existingBooks = books?.Where(i => i.Versions?.Any(i => i.Path == path) ?? false).FirstOrDefault();
+
+            string? folderPath = null;
+
+            if (!string.IsNullOrEmpty(folderTitle))
+            {
+                folderPath = path.Replace($"/{title}.{extension}", "");
+                existingBooks = books?.Where(i => i.FolderPath == folderPath).FirstOrDefault();
+            }
+
+            if (existingBooks != null)
+            {
+                if (!string.IsNullOrEmpty(folderTitle))
+                {
+                    var version = new InventoryItemVersion
+                    {
+                        Id = Guid.NewGuid(),
+                        Path = path,
+                    };
+
+                    if (existingBooks.Versions?.Any(i => i.Path == path) ?? false)
+                    {
+                        return;
+                    }
+
+                    // Do this after the path check because a file info will be created
+                    version.FileInfoId = (await _fileInfoService.CreateFileInfo(path, version.Id, "Audiobook"))?.Id;
+
+                    existingBooks.Versions = existingBooks.Versions?.Append(version);
+
+                    await _inventoryService.UpdateByTitle(existingBooks);
+                }
+
+                return;
+            }
+
+            var versionId = Guid.NewGuid();
+            var audiobook = new Audiobook()
+            {
+                Id = Guid.NewGuid(),
+                Versions =
+                [
+                    new()
                 {
                     Id = versionId,
                     Path = path,
                     FileInfoId = (await _fileInfoService.CreateFileInfo(path, versionId, "Audiobook"))?.Id
                 }
-            ],
-            Title = title,
-            FolderPath = folderPath
-        };
+                ],
+                Title = title,
+                FolderPath = folderPath
+            };
 
-        var metadata = await _metadataService.CreateNewMetadata
-        (
-            parentId: book.Id,
-            title: book.Title,
-            category: book.Category,
-            year: groups.TryGetValue("year", out var movieTitleYear) ?
-                    movieTitleYear.Value : groups.TryGetValue("folderYear", out var movieFolderYear) ?
-                    movieFolderYear.Value : null
-        );
+            var metadata = await _metadataService.CreateNewMetadata
+            (
+                parentId: audiobook.Id,
+                title: audiobook.Title,
+                category: audiobook.Category
+            );
 
-        book.MetadataId = metadata?.Id;
+            audiobook.MetadataId = metadata?.Id;
 
-        await _inventoryService.AddItem(book);
+            await _inventoryService.AddItem(audiobook);
+        }
+    }
+
+    private bool IsPart(string path)
+    {
+        // TODO
+        return false;
     }
 }
